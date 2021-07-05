a71597a33efd46ef99e736e37c4c16ad5c57e66c
osanseviero <osanseviero@gmail.com>
2021-07-05 15:25:35 +0200

    Format

--- src/spacy_huggingface_hub/push_cli.py
+++ src/spacy_huggingface_hub/push_cli.py
@@ -12,7 +12,7 @@ from spacy.cli._util import parse_config_overrides, Arg, Opt, app
 import logging
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 SPACY_HF_HUB_HELP = """CLI for uploading pipelines to the 
 Hugging Face Hub (hf.co).
@@ -24,8 +24,11 @@ DOCS: https://spacy.io/api/cli
 """
 
 # Create our subcommand, and install it within spaCy's CLI
-hf_hub_cli = typer.Typer(name="huggingface_hub", help=SPACY_HF_HUB_HELP, no_args_is_help=True)
+hf_hub_cli = typer.Typer(
+    name="huggingface_hub", help=SPACY_HF_HUB_HELP, no_args_is_help=True
+)
 app.add_typer(hf_hub_cli)
+
 
 @hf_hub_cli.command(
     "push", context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
@@ -39,19 +42,19 @@ def huggingface_hub_push_cli(
     """
     Push a spaCy pipeline to the Hugging Face Hub.
     """
-    push(
-        whl_path,
-        organization
-    )
+    push(whl_path, organization)
+
 
 token_classification_components = ["ner", "tagger", "morphologizer"]
 text_classification_components = ["textcat", "textcat_multilabel"]
+
 
 def _insert_value(metadata, name, value):
     if value is None or value == "":
         return metadata
     metadata[name] = value
     return metadata
+
 
 def _insert_values_as_list(metadata, name, values):
     if values is None:
@@ -63,12 +66,10 @@ def _insert_values_as_list(metadata, name, values):
     metadata[name] = values
     return metadata
 
-def _create_metric(name, t, value): 
-    return {
-            "name": name,
-            "type": t,
-            "value": value
-        }
+
+def _create_metric(name, t, value):
+    return {"name": name, "type": t, "value": value}
+
 
 def _create_p_r_f_list(precision, recall, f_score):
     precision = _create_metric("Precision", "precision", precision)
@@ -77,50 +78,71 @@ def _create_p_r_f_list(precision, recall, f_score):
 
     return [precision, recall, f_score]
 
+
 def _create_model_index(repo_name, data):
     model_index = {"name": repo_name}
 
     results = []
     if "ents_p" in data:
-        results.append({
-            "tasks": {
-                "name": "NER",
-                "type": "token-classification",
-                "metrics": _create_p_r_f_list(data["ents_p"], data["ents_r"], data["ents_f"])
+        results.append(
+            {
+                "tasks": {
+                    "name": "NER",
+                    "type": "token-classification",
+                    "metrics": _create_p_r_f_list(
+                        data["ents_p"], data["ents_r"], data["ents_f"]
+                    ),
+                }
             }
-        })
+        )
     if "tag_acc" in data:
-        results.append({
-            "tasks": {
-                "name": "POS",
-                "type": "token-classification",
-                "metrics": [_create_metric("Accuracy", "accuracy", data["tag_acc"])]
+        results.append(
+            {
+                "tasks": {
+                    "name": "POS",
+                    "type": "token-classification",
+                    "metrics": [
+                        _create_metric("Accuracy", "accuracy", data["tag_acc"])
+                    ],
+                }
             }
-        })
+        )
     if "sents_p" in data:
-        results.append({
-            "tasks": {
-                "name": "SENTER",
-                "type": "token-classification",
-                "metrics": _create_p_r_f_list(data["sents_p"], data["sents_r"], data["sents_f"])
+        results.append(
+            {
+                "tasks": {
+                    "name": "SENTER",
+                    "type": "token-classification",
+                    "metrics": _create_p_r_f_list(
+                        data["sents_p"], data["sents_r"], data["sents_f"]
+                    ),
+                }
             }
-        })
+        )
     if "dep_uas" in data:
-        results.append({
-            "tasks": {
-                "name": "UNLABELED_DEPENDENCIES",
-                "type": "token-classification",
-                "metrics": [_create_metric("Accuracy", "accuracy", data["dep_uas"])]
+        results.append(
+            {
+                "tasks": {
+                    "name": "UNLABELED_DEPENDENCIES",
+                    "type": "token-classification",
+                    "metrics": [
+                        _create_metric("Accuracy", "accuracy", data["dep_uas"])
+                    ],
+                }
             }
-        })
+        )
     if "dep_las" in data:
-        results.append({
-            "tasks": {
-                "name": "LABELED_DEPENDENCIES",
-                "type": "token-classification",
-                "metrics": [_create_metric("Accuracy", "accuracy", data["dep_uas"])]
+        results.append(
+            {
+                "tasks": {
+                    "name": "LABELED_DEPENDENCIES",
+                    "type": "token-classification",
+                    "metrics": [
+                        _create_metric("Accuracy", "accuracy", data["dep_uas"])
+                    ],
+                }
             }
-        })
+        )
 
     model_index["results"] = results
     return model_index
@@ -133,19 +155,25 @@ def _create_model_card(repo_name, repo_dir):
         lic = data.get("license", "").replace(" ", "-")
         tags = ["spacy"]
         for component in data["components"]:
-            if component in token_classification_components and "token-classification" not in tags:
+            if (
+                component in token_classification_components
+                and "token-classification" not in tags
+            ):
                 tags.append("token-classification")
-            if component in text_classification_components and "text-classification" not in tags:
+            if (
+                component in text_classification_components
+                and "text-classification" not in tags
+            ):
                 tags.append("text-classification")
 
         metadata = _insert_values_as_list({}, "tags", tags)
         metadata = _insert_values_as_list(metadata, "language", lang)
         metadata = _insert_value(metadata, "license", lic)
-        metadata["model-index"]  = _create_model_index(repo_name, data["performance"])
+        metadata["model-index"] = _create_model_index(repo_name, data["performance"])
         metadata = yaml.dump(metadata, sort_keys=False)
         metadata_section = f"---\n{metadata}---\n"
 
-        # Read README generated by package
+        # Read README generated by package
         readme_path = os.path.join(repo_dir, "README.md")
         readme = ""
         if os.path.isfile(readme_path):
@@ -159,8 +187,8 @@ def _create_model_card(repo_name, repo_dir):
 
 def push(
     whl_path,
-    namespace: Optional[str]=None,
-):  
+    namespace: Optional[str] = None,
+):
     filename = os.path.basename(whl_path)
     repo_name, version, _, _, _ = filename.split("-")
     versioned_name = repo_name + "-" + version
@@ -169,25 +197,25 @@ def push(
     # Create the repo (or clone its content if it's nonempty)
     api = HfApi()
     repo_url = api.create_repo(
-            name=repo_name,
-            token=HfFolder.get_token(),
-            organization=namespace,
-            private=False,
-            exist_ok=True,
-        )
+        name=repo_name,
+        token=HfFolder.get_token(),
+        organization=namespace,
+        private=False,
+        exist_ok=True,
+    )
     repo = Repository(repo_local_path, clone_from=repo_url)
     repo.git_pull(rebase=True)
     repo.lfs_track(["*.whl", "*.npz", "*strings.json", "vectors"])
 
     # Extract information from whl file
     logger.debug("Extracting information from .whl file.")
-    with zipfile.ZipFile(whl_path, 'r') as zip_ref:
+    with zipfile.ZipFile(whl_path, "r") as zip_ref:
         base_name = os.path.join(repo_name, versioned_name)
         for file_name in zip_ref.namelist():
             if file_name.startswith(base_name):
-                zip_ref.extract(file_name, 'hub')
+                zip_ref.extract(file_name, "hub")
 
-    # Move files up one directory
+    # Move files up one directory
     extracted_dir = os.path.join(repo_local_path, versioned_name)
     for filename in os.listdir(extracted_dir):
         dst = os.path.join(repo_local_path, filename)
@@ -201,15 +229,14 @@ def push(
     # Create model card, including HF tags
     _create_model_card(repo_name, repo_local_path)
 
-    # Remove version from whl filename
+    # Remove version from whl filename
     dst_file = os.path.join(repo_local_path, f"{repo_name}-any-py3-none-any.whl")
     shutil.copyfile(whl_path, dst_file)
 
     logger.debug("Pushing repository to the Hub.")
     url = repo.push_to_hub(commit_message="Spacy Update")
     url, _ = url.split("/commit/")
-    
+
     logger.info("View your model here: {}".format(url))
     whl_path = f"{url}/resolve/main/{repo_name}-any-py3-none-any.whl"
     logger.info(f"Install your model: pip install {whl_path}")
-    
